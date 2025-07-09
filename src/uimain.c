8454f9e6c7cdc85ad1f73ffbfda09ce2db103035
Nocturnevh <vaherrera6@miners.utep.edu>
2025-07-09 14:29:22 -0600

    Feature: Added history functionality to the uimain.c. Fix: Minor fixes on uimain text and promts.

--- src/uimain.c
+++ src/uimain.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
 
 #define MAX 100
 
 /* Simple user interface for Section A Lab 1 */
 int main(){
   char str[MAX];
-  printf("Welcome to my tokenizer, type a string and watch it be tokenized. To exit type !q.\n");
+  List *history = init_history();
+
+  
+  printf("Welcome to my tokenizer, type a string and watch it be tokenized.\n");
+  printf("To view history !h.\nTo view num history  type ! followed by number 0-9.\n");
   
   while(1){
     printf("$ ");
@@ -24,10 +29,44 @@ int main(){
       printf("Goodbye.\n");
       break;
     }
+    /*Handle history display using !h*/
+    if(str[0] == '!' && str[1] == 'h'){
+      printf("History:\n");
+      print_history(history);
+
+    }
+
+    else if(str[0] == '!' && str[1] >= '0' && str[1] <= '9'){
+      int id = str[1] - '0';
+      char *recalled_str = get_history(history, id);
+      if(recalled_str){
+	printf("Recalling: %s\n", recalled_str);
+	char **tokens = tokenize(recalled_str);
+	print_tokens(tokens);
+	free_tokens(tokens);
+      }
+      else{
+	printf("No history item found with id %d\n", id);
+      }
+    }
+    else{
+      int i = 0;
+      while(str[i] != '\0'){
+	if(str[i] == '\n'){
+	  str[i] = '\0';
+	  break;
+	}
+	i++;
+      }
+    
+
+    add_history(history, str);
 
     char **tokens = tokenize(str);
     print_tokens(tokens);
     free_tokens(tokens);
+    }
   }
+  free_history(history);
   return 0;
 }
